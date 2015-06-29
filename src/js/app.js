6ff3123d7e51fd721ac6d16ec84e3f3ef4edf6ce
Andrew Sass <andrewpsass@gmail.com>
2015-06-29 06:20:48 -0400

    designz

--- src/js/app.js
+++ src/js/app.js
@@ -20,3 +20,20 @@ $(".icon").click(function () {
 });
 
 
+// Short script to encode SVG in base64
+// This can be reversed using window.atob('base64')
+var code = document.getElementById('code');
+var demo = document.getElementById('demo');
+var svg = document.getElementsByTagName('svg')[0];
+
+// Convert the SVG node to HTML.
+var div = document.createElement('div');
+div.appendChild(svg.cloneNode(true));
+
+// Encode the SVG as base64
+var b64 = 'data:image/svg+xml;base64,'+window.btoa(div.innerHTML);
+var url = 'url("' + b64 + '")';
+code.innerHTML = 'Base64 CSS (for cross-browser compatibility)\n\nbackground-image: ' + url + ';';
+demo.style.backgroundImage = url;
+
+
